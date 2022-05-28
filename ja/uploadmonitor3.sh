c9fdb2ba08a1d0a46a9cad684b87e7b3d64762ab
Yusuke Takeda <61795460+ytakeda-paleo@users.noreply.github.com>
2022-05-28 21:21:28 +0900

    Create uploadmonitor3.sh

---
+++ ja/uploadmonitor3.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+#configファイルの読み込み
+. ./uploadmonitor-config.sh
+
+#Chartwork送信の関数
+function chatwork(){
+curl -X POST -H "X-ChatWorkToken: $CHATWORK_API_KEY" --data-urlencode "body=$TEXT" "https://api.chatwork.com/v2/rooms/$CHATWORK_ROOM_ID/messages"
+}
+
+#ファイル数確認の関数
+function checkimages(){
+cd ${DIR_PATH[3]}
+#LATESTFILES=`ls -l | wc -l` <<< too heavy
+#LATESTFILES=`find -type f | wc -l` <<< error at MacOS terminal
+LATESTFILES=`find . -type f | wc -l`
+if [ "$LATESTFILES" -le "$PREVIOUSFILES" ]; then
+TEXT="警告：${TIME[3]} 分以上画像の追加がありません．フォルダ：3"
+chatwork
+cd 
+echo $LATESTFILES > ${FILE_LOG[3]}
+else
+cd 
+echo $LATESTFILES > ${FILE_LOG[3]}
+fi
+}
+
+#監視対象フォルダの存在確認
+if [ ! -d "${DIR_PATH[3]}" ]; then
+TEXT="監視に失敗しました：保存先フォルダが見つかりません．保存先フォルダを指定しなおしてください．フォルダ：3"
+chatwork
+exit
+else
+:
+fi
+
+#LOGファイルの存在確認
+if [ ! -f "${FILE_LOG[3]}" ]; then
+TEXT="監視に失敗しました：logファイルが見つかりません．管理者に問い合わせてください．フォルダ：3"
+chatwork
+exit
+else
+#LOGファイルの読み込み
+PREVIOUSFILES=$(<${FILE_LOG[3]})
+fi
+
+checkimages
+
+if [ $? -gt 0 ]; then
+TEXT="監視に失敗しました：uploadmonitor1.shが実行できませんでした．管理者に問い合わせてください．フォルダ：1"
+chatwork
+fi
